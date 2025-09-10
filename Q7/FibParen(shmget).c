a00b1dbcfae653a3699fa792d68f0751a675f3fb
Aneesh S <aneeshappid@gmail.com>
2025-09-10 20:16:42 +0530

    Improve input validation and fork handling
    Refactor shared memory handling and input validation.

--- Q7/FibParen(shmget).c
+++ Q7/FibParen(shmget).c
@@ -4,30 +4,59 @@
 #include <sys/types.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
-  int n1, n2, n3, k = 2;
+  int i;
   void *ptr;
-  int shmid = shmget((key_t)1122, 4096, 0666);
-  ptr = shmat(shmid, NULL, 0666);
-  int i = atoi(argv[1]);
-  n1 = 0;
-  n2 = 1;
-  sprintf(ptr, "%d", n1);
-  ptr += strlen(ptr);
-  sprintf(ptr, "%d", n2);
-  ptr += strlen(ptr);
-  while (k != i)
+  int shmid = shmget((key_t)1122, 4096, 0666 | IPC_CREAT);
+  ptr = shmat(shmid, NULL, 0);
+
+  if (argc == 2)
   {
-    n3 = n1 + n2;
-    sprintf(ptr, "%d", n3);
-    ptr += strlen(ptr);
-    n1 = n2;
-    n2 = n3;
-    k++;
+    sscanf(argv[1], "%d", &i);
+    if (i < 1)
+    {
+      printf("Error input: %d\n", i);
+      return 0;
+    }
   }
-  ptr += strlen(ptr);
-  shmctl(shmid, IPC_RMID, NULL);
+  else if (argc > 2)
+  {
+    printf("Too many arguments\n");
+    return 1;
+  }
+  else
+  {
+    printf("Invalid input format\n");
+    return 1;
+  }
+
+  pid_t pid;
+  pid = fork();
+  if (pid == 0)
+  {
+    execlp("./child", "child", argv[1], NULL);
+    perror("execlp failed");
+    exit(1);
+  }
+  else if (pid > 0)
+  {
+    wait(NULL);
+    printf("\nPARENT: child completed\n");
+    printf("Parent printing Fibonacci series:\n");
+    printf("%s\n", (char *)ptr);
+
+    shmdt(ptr);                 // detach
+    shmctl(shmid, IPC_RMID, 0); // remove shared memory
+  }
+  else
+  {
+    perror("fork failed");
+    return 1;
+  }
+
   return 0;
 }
